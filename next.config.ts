963ef78a59f93824799ee16e3f75dde99d993b4f
Aleksandr <needspeed@bk.ru>
2026-04-25 20:45:53 +0300

    Добавлены статические файлы сайта в корень

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.101'],
+  output: 'export',  // Добавьте эту строку
+  images: {
+    unoptimized: true,  // Добавьте эту строку для работы изображений
+  },
 };
 
 export default nextConfig;
