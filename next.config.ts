04610568e2ea54aee179daa883e20e76b06371c5
Aleksandr <needspeed@bk.ru>
2026-04-26 13:27:14 +0300

    fix: image paths and rebuild

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.101'],
-  output: 'export',  // Добавьте эту строку
+  output: 'export',
+  basePath: '/siteyar',
   images: {
-    unoptimized: true,  // Добавьте эту строку для работы изображений
+    unoptimized: true,
   },
 };
 
